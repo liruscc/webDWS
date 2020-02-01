aae74f8c16894f342b03bd25c484400ebb8eac67
Laura Ortiz <lauraortizluque@gmail.com>
2020-02-01 20:08:17 +0100

    mover main a views

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once ('plantilla/nav.php');
 <div id="container" class="d-flex flex-row col-lg-12 col-md-12 col-sm-12 justify-content-center">
     <?php
     require_once ('plantilla/leftmenu.php');
-    require_once ('plantilla/main.php');
+    require_once ('views/main.php');
     require_once ('plantilla/rightmenu.php');
     ?>
 </div>
