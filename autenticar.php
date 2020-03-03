5d91d84cf9f48b50326589f7b0fcfa6a37190061
Laura Ortiz <lauraortizluque@gmail.com>
2020-03-04 00:51:25 +0100

    control usuarios bloqueados

--- autenticar.php
+++ autenticar.php
@@ -1,34 +1,40 @@
 <?php
-		require_once('classes/cliente.php');
-		session_start();
 
-		//Login
-		if($_POST){
+require_once('classes/cliente.php');
+session_start();
 
-			$usuario= new Cliente();
+//Login
+if ($_POST) {
 
- 			$errores = cliente::validarLogin($_POST['usuario'],$_POST['password']);
- 			if(!$errores){
- 				$usuario=Cliente::verificarPassword($_POST['usuario'],$_POST['password']);
-				// si el id del objeto retornado no es null, quiere decir que encontro un registro en la base
-				if ($usuario) {
-					$_SESSION['id']=$usuario->getDni(); 
-					$_SESSION['tipo']=$usuario->getTipo();
-					header('Location: index.php?correcto'); 
-				}else{
-					session_destroy();
-					header('Location: index.php?error=El usuario o parssword no son correctos.'); // cuando los datos son incorrectos envia a la página de error
-				}	
- 			}else{
- 				session_destroy();
-					header('Location: index.php?error=El usuario o parssword no son correctos.'); // cuando los datos son incorrectos envia a la página de error
- 			}		
-		}
-		
-		//Logout
-		if(isset($_GET['logout'])){
-			session_destroy();
-			header('Location: index.php'); // cuando los datos son incorrectos envia a la 
-		}
-	
+    $usuario = new Cliente();
+
+    $errores = cliente::validarLogin($_POST['usuario'], $_POST['password']);
+    if (!$errores) {
+        $usuario = Cliente::verificarPassword($_POST['usuario'], $_POST['password']);
+        // si el id del objeto retornado no es null, quiere decir que encontro un registro en la base
+        if ($usuario->getActivo() == '1') {
+
+            if ($usuario) {
+                $_SESSION['id'] = $usuario->getDni();
+                $_SESSION['tipo'] = $usuario->getTipo();
+                header('Location: index.php?correcto');
+            } else {
+                session_destroy();
+                header('Location: index.php?error=El usuario o parssword no son correctos.'); // cuando los datos son incorrectos envia a la página de error
+            }
+        } else {
+            session_destroy();
+            header('Location: index.php?error=El usuario está bloqueado.'); // cuando los datos son incorrectos envia a la página de error
+        }
+    } else {
+        session_destroy();
+        header('Location: index.php?error=El usuario o parssword no son correctos.'); // cuando los datos son incorrectos envia a la página de error
+    }
+}
+
+//Logout
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header('Location: index.php'); // cuando los datos son incorrectos envia a la 
+}
 ?>
